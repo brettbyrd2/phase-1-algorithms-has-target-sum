92ef418d57d55eac02fcefe118376de5554731a2
Brett Byrd <byrdb2@yahoo.com>
2023-04-03 18:10:22 -0500

    complete

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  let result = []
+   for (let i = 0; i < array.length; i++){
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j] === target - array[i]){
+        result.push([array[i], array[j]])
+        return true
+      }
+    }
+  }
+  return false
+}   
+  
+
+  
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(n*n)
 */
 
 /* 
   Add your pseudocode here
+  set up a function that returns true if any pair of numbers adds up to the target
+  grab the first number in an array and look through the rest of the array for any number
+  that adds up to target. If you find a number that adds up to the target return true and push
+  those two numbers into a new array. Basically I want to iterate through the array and then for each element
+  in the array I want to iterate through the array again to find any number that adds up to the target.
 */
 
 /*
   Add written explanation of your solution here
+Set up an empty array to the result variable then loop and begin to iterate through our array.
+
+then make a second for loop and set the index variable j to i + 1
+
+check to see if the element in the second loop is equal to the sum minus the element in the first loop.
+
+if true, push the two numbers as a sub-array into the results array.
+
+else return false
 */
 
 // You can run `node index.js` to view these console logs
